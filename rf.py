b8c1be792491c6d7f21855bf1639e65f346e2323
thund3rcat <michael.hengeler@gmx.de>
2016-05-12 20:44:27 +0200

    Refactor rf.py: add function rpi_get_power Build module rpi_script.py: routine for raspberry

--- rf.py
+++ rf.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from rtlsdr import *
 from scipy import signal
-        
+
 class RFear:
     """A simple class to compute PSD with a DVBT-dongle"""
     # Statisches Attribut / Klassenattribut
@@ -16,7 +16,7 @@ class RFear:
         self.__sdr.sample_rate = 2.4e6        
         self.__sdr.gain = 1
         self.__freq = []
-        self.set_freq(self,*args)
+        self.set_freq(self, *args)
     
     # Destruktor
     def __del__(self):
@@ -26,11 +26,11 @@ class RFear:
     def get_freq(self):
         return self.__freq
 
-    def set_freq(self,*args):  
+    def set_freq(self, *args):  
         self.__freq[:] = []
         for arg in args:
             self.__freq.append(arg)
-        if not isinstance(self.__freq[0],float):
+        if not isinstance(self.__freq[0], float):
             self.__freq = self.__freq[1:]
         self.__sdr.center_freq = np.mean(self.__freq)
               
@@ -55,30 +55,30 @@ class RFear:
                 print "Liveplot interrupted by user"
                 drawing = False
 
-    def find_peaks(self,power,freqs,interval = 80):
+    def find_peaks(self, power, freqs, interval=80):
         l = len(self.__freq)
         power = power.tolist()
         freqsTemp = freqs.tolist()
         freqsTemp = [float(xx) for xx in freqs]
-        freqsTemp = [round(xxx,1) for xxx in freqs]
-        print "\n Freq Temp: \n"
-        print freqsTemp 
-        print "\n Length of Freq vector: \n"
-        print len(freqs)
+        freqsTemp = [round(xxx, 1) for xxx in freqs]
+        # print "\n Freq Temp: \n"
+        # print freqsTemp 
+        # print "\n Length of Freq vector: \n"
+        # print len(freqs)
         pmax = []
         for i in range(l):
             x = freqsTemp.index(self.__freq[i]/1e6)
-            pmax.append(max(power[x : x + interval]))
+            pmax.append(max(power[x: x + interval]))
         return pmax
-    def find_peaks2(self,power,freqs,interval = 20):
+    def find_peaks2(self, power, freqs, interval=20):
         return max(power)  
 
     # measure power values of specified frequency for a certain time
-    def get_power(self, time = 10, size = 256):
+    def get_power(self, time=10, size=256):
         elapsed_time = 0      
         powerstack = []
         timestack = []      
-        while elapsed_time < time :  
+        while elapsed_time < time: 
             start_calctime = t.time()
 	    # read samples    
             samples = self.__sdr.read_samples(size * 1024)
@@ -105,33 +105,41 @@ class RFear:
         print elapsed_time 
         plt.clf()
         plt.axis([0, len(powerstack), -50, 30])
-        plt.plot(10*np.log10(powerstack),"o")
+        plt.plot(10*np.log10(powerstack), "o")
         plt.xlabel("Updates")
         plt.ylabel("Maximum power (dB)")
         plt.figure()
-        plt.plot(timestack,"g^")
+        plt.plot(timestack, "g^")
         plt.xlabel("Updates")
         plt.ylabel("Computation time (sec)")
         plt.show()
         return powerstack
 
-    def write_to_file(l, text, filename = "Experiments"):
-        f  = open(filename,"a")
+    def rpi_get_power(self, printing=0, size=256):
+        #"""Routine for Raspberry Pi: """
+        running = True
+        pmax = []
+        while running:
+            try:
+                samples = self.__sdr.read_samples(size * 1024)        
+                power, freqs = plt.psd(samples, NFFT=1024, Fs=self.__sdr.sample_rate/1e6, Fc=self.__sdr.center_freq/1e6)
+                pmax.append(self.find_peaks(power, freqs))
+                if printing:
+                    print self.find_peaks(power, freqs)
+                    print "\n"
+                else:
+                    pass
+            except KeyboardInterrupt:
+                print "Process interrupted by user"
+                return pmax
+                running = False
+        
+
+    def write_to_file(self, l, text, filename="Experiments"):
+        f = open(filename, "a")
         f.write(t.ctime() + "\n")
         f.write(text + "\n")
         f.write(str(l))
         f.write("\n\n")
         f.close()
-        
-if __name__ == "__main__":
-    import sys
-    import rf
-    ear = rf.RFear(float(sys.argv[1]))
-
-
-
-    
-    
-    
-    
         
---
+++ rpi_script.py
@@ -0,0 +1,8 @@
+import rf
+
+ear = rf.RFear(435e6)
+
+state = True
+
+while state:
+    ear.rpi_get_power()
