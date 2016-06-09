76adac10c05a856bdaffe1740e6b871a3ed04d99
thund3rcat <michael.hengeler@gmx.de>
2016-06-09 18:45:30 +0200

    Refactor rf.py: add method for calibration and optimal parameter estimation

--- rf.py
+++ rf.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from rtlsdr import *
 from scipy import signal
+from scipy.optimize import curve_fit
 
 class RFear(object):
     """A simple class to compute PSD with a DVBT-dongle."""
@@ -22,6 +23,8 @@ class RFear(object):
         self.__sdr.gain = 1
         self.__freq = []
         self.set_freq(self, *args)
+        self.__d0 = np.array(0, dtype=float)
+        self.__p0 = np.array(0, dtype=float)
 
     def __del__(self):
         del self.__sdr
@@ -66,6 +69,19 @@ class RFear(object):
         print 'Current sample rate: '
         return self.__sdr.sample_rate
 
+    def calibrate(self):
+        """Set reference values P0 and d0"""
+        d_ref = raw_input('Please enter distance'
+            'from transmitter to receiver [cm]: ')
+        self.__d0 = np.array(d_ref, dtype=float)
+        p_ref, variance = self.make_test(30)
+        del variance
+        self.__p0 = np.array(p_ref, dtype=float)
+
+    def get_caldata(self):
+        """Returns the reference values from calibration"""
+        return self.__d0, self.__p0
+
     def get_psd(self, size=256):
         """Get Power Spectral Density Live Plot."""
         plt.ion()      # turn interactive mode on
@@ -81,11 +97,12 @@ class RFear(object):
                 # use matplotlib to estimate and plot the PSD
                 #plt.psd(samples, NFFT=1024, Fs=self.__sdr.sample_rate/1e6,
                 #    Fc=self.__sdr.center_freq/1e6)
-                f, Pxx_den = signal.periodogram(samples,
+                freq, pxx_den = signal.periodogram(samples,
                     fs=self.__sdr.sample_rate/1e6, nfft=1024)
-                plt.plot(f+self.__sdr.center_freq/1e6, 10*np.log10(Pxx_den))
+                plt.plot(freq+self.__sdr.center_freq/1e6, 10*np.log10(pxx_den))
+                plt.grid()
                 plt.xlabel('Frequency (MHz)')
-                plt.ylabel('Relative power (dB)')
+                plt.ylabel('Relative power (dBm)')
                 plt.draw()
                 t.sleep(0.01)
             except KeyboardInterrupt:
@@ -123,10 +140,11 @@ class RFear(object):
         print 'Total time (sec):\n'
         print elapsed_time
         plt.clf()
+        plt.grid()
         plt.axis([0, len(powerstack), -50, 30])
         plt.plot(10*np.log10(powerstack), 'o')
         plt.xlabel('Updates')
-        plt.ylabel('Maximum power (dB)')
+        plt.ylabel('Maximum power (dBm)')
         plt.figure()
         plt.plot(timestack, 'g^')
         plt.xlabel('Updates')
@@ -179,16 +197,13 @@ class RFear(object):
             return max(pmax), freqmax[pmax.index(max(pmax))]
         
     def find_peaks2(self, size=256):
-        length = len(self.__freq)
+        #length = len(self.__freq)
         pmax = []
         samples = self.__sdr.read_samples(size * 1024)
-        f, Pxx_den = signal.periodogram(samples,
+        freq, pxx_den = signal.periodogram(samples,
             fs=self.__sdr.sample_rate/1e6, nfft=1024)
-        for i in range(length):
-            pmax.append(max(Pxx_den))
+        pmax.append(max(pxx_den))
         return pmax
-        
-
 
     def rpi_get_power(self, printing=0, size=256):
         """Routine for Raspberry Pi.
@@ -209,29 +224,45 @@ class RFear(object):
                 print 'Process interrupted by user'
                 return pmax
 
-    def make_test(self, size=256):
+    def make_test(self, time=10, size=256):
         """Interactive method to get PSD data
         at characteristic frequencies.
         """
         testing = True
-        powerstack = []
+        modeldata = []
+        variance = []
         while testing:
             try:
-                raw_input('Press Enter to make a measurement'
+                raw_input('Press Enter to make a measurement,'
                     ' or Ctrl+C+Enter to stop testing:\n')
-                print ' ... '
-                powerstack.append(self.find_peaks(size))
+                elapsed_time = 0
+                powerstack = []
+                print ' ... measuring ...'
+                while elapsed_time < time:
+                    start_calctime = t.time()
+                    powerstack.append(self.find_peaks2(size))
+                    calc_time = t.time() - start_calctime
+                    elapsed_time = elapsed_time + calc_time
+                    t.sleep(0.01)
                 print 'done\n'
-                t.sleep(0.005)
+                t.sleep(0.5)
+                print ' ... evaluating ...'
+                modeldata.append(np.mean(powerstack))
+                variance.append(np.var(powerstack))
+                del powerstack
+                print 'done\n'
+                t.sleep(0.5)
             except KeyboardInterrupt:
                 print 'Testing finished'
                 testing = False
         plt.figure()
-        plt.axis([0, len(powerstack), -50, 30])
-        plt.plot(10*np.log10(powerstack), 'o')
-        plt.xlabel('Updates')
-        plt.ylabel('Maximum power (dB)')
-        return powerstack
+        plt.grid()
+        #plt.axis([0, len(modeldata), -50, 30])
+        plt.errorbar(range(len(modeldata)), modeldata, yerr=variance,
+            fmt='o', ecolor='g')
+        plt.xlabel('Evaluations')
+        plt.ylabel('Mean Maximum power [?]')
+        return modeldata, variance
 
     def get_performance(self, bandwidth=2.4e6):
         """Measure performance at certain sizes and sampling rates
@@ -270,27 +301,56 @@ class RFear(object):
         print 'Total time [sec]: '
         print total_time
         plt.figure()
+        plt.grid()
         plt.plot(SIZE, VAR, 'ro')
         plt.xlabel('Sample Size (*1024)')
         plt.ylabel('Variance (dB)')
         plt.figure()
+        plt.grid()
         plt.plot(SIZE, MEAN, 'x')
         plt.xlabel('Sample Size (*1024)')
         plt.ylabel('Mean Value (dB)')
         plt.figure()
+        plt.grid()
         plt.plot(SIZE, UPDATE, 'g^')
         plt.xlabel('Sample Size (*1024)')
         plt.ylabel('Update rate (sec)')
         plt.show()
         return SIZE, VAR, MEAN, UPDATE
 
+    def get_model(self, ddata, pdata, vdata):
+        """Create a function to fit with measured data.
+        alpha and xi are the coefficients that curve_fit will calculate.
+        The function structure is known and some coeffs are known
+        through calibration.
+        """
+        ddata = np.array(ddata, dtype=float)
+        pdata = np.array(pdata, dtype=float)
+        vdata = np.array(vdata, dtype=float)
+        plt.grid()
+        plt.errorbar(ddata, pdata, yerr=vdata,
+                fmt='ro', ecolor='g', label='Original Data')
+        def func(ddata, alpha, xi):
+            d_ref, p_ref = self.get_caldata()
+            return p_ref-10*alpha*np.log10(ddata/d_ref)+xi
+        popt, pcov = curve_fit(func, ddata, pdata)
+        del pcov
+        print 'alpha = %s , xi = %s' % (popt[0], popt[1])
+        ddata = np.linspace(ddata[0], ddata[-1], num=1000)
+        plt.plot(ddata,func(ddata, *popt), label='Fitted Curve')
+        plt.legend(loc='upper right')
+        plt.show()
+        return popt
+
+
 def plot_result(results):
     """Plot results extracted from textfile"""
     plt.figure()
+    plt.grid()
     plt.axis([0, len(results), -50, 30])
     plt.plot(10*np.log10(results), 'o')
     plt.xlabel('Updates')
-    plt.ylabel('Maximum power (dB)')
+    plt.ylabel('Maximum power (dBm)')
     plt.show()
 
 def write_to_file(results, text, filename='Experiments'):
@@ -306,3 +366,5 @@ def write_to_file(results, text, filename='Experiments'):
     datei.write(str(results))
     datei.write('\n\n')
     datei.close()
+
+
